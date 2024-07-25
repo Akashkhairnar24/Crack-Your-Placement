1226309ea7e92023613f8ec6232aad0c7c282c1a
Akashkhairnar24 <137057113+Akashkhairnar24@users.noreply.github.com>
2024-07-25 23:43:09 +0530

    Add files via upload

---
+++ Day-11/Implement_stack_using_queue.cpp
@@ -0,0 +1,41 @@
+class MyStack {
+private:
+ queue<int> q1;
+public:
+    MyStack() {
+       
+    }
+    
+    void push(int x) {
+        int k = q1.size();
+        if(k==0)
+        {
+            q1.push(x);
+        }
+        else
+        {
+            q1.push(x);
+            for(int i=0;i<k;i++)
+            {
+                int m = q1.front();
+                q1.pop();
+                q1.push(m);
+            }
+        }
+    }
+    
+    int pop() {
+        if(q1.empty()) return -1;
+        int m = q1.front();
+        q1.pop();
+        return m;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
