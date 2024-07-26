9066f9a6ee25038f79b151254dcd54274886f99a
Akashkhairnar24 <137057113+Akashkhairnar24@users.noreply.github.com>
2024-07-26 23:42:33 +0530

    Add files via upload

---
+++ Day-12/Online_stock_span.cpp
@@ -0,0 +1,12 @@
+class StockSpanner {
+public:
+    stack<pair<int, int>> s;
+    StockSpanner() {}
+    
+    int next(int price) {
+        int span = 1;
+        while(s.size() && s.top().first <= price) span += s.top().second, s.pop();
+        s.push({price, span});
+        return span;
+    }
+};
